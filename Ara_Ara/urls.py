7abcc6e20c3ebea16a9608b62680ace5dffda55c
ksdfg <ksdfg123@gmail.com>
2019-12-22 16:50:09 +0530

    added user table in database
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- Ara_Ara/urls.py
+++ Ara_Ara/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from Ara_Ara import views
 
 urlpatterns = [
-    path('', views.hello),
-    path('hello', views.hello)
+    path('', views.hello, name='homepage'),
+    path('hello', views.hello, name='hello')
 ]
