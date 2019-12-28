8d135ea2f96d89cdf50af72bd10a23716fa76b2c
ksdfg <ksdfg123@gmail.com>
2019-12-28 23:37:06 +0530

    add name to db models, otherwise they were named Ara_Ara_db_name
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- Ara_Ara/models.py
+++ Ara_Ara/models.py
@@ -5,5 +5,7 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=50)
-    name = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'User'
