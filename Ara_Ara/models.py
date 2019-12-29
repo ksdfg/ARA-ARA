b90a4d475223ccc0114bfbf5ba11fbc8281f35f2
ksdfg <ksdfg123@gmail.com>
2019-12-29 14:25:25 +0530

    add anime table to Ara_Ara database
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- Ara_Ara/models.py
+++ Ara_Ara/models.py
@@ -4,8 +4,19 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    username = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, primary_key=True)
     password = models.CharField(max_length=100)
 
     class Meta:
         db_table = 'User'
+
+
+class Anime(models.Model):
+    name = models.CharField(max_length=100)
+    total_eps = models.IntegerField()
+    aired_eps = models.IntegerField(null=True)
+    status = models.CharField(max_length=1,
+                              choices=(('a', "airing"), ('n', "not yet aired"), ('f', "finished airing")))
+
+    class Meta:
+        db_table = 'Anime'
