75c51cd24eebdc69613aa01c9b1d7d93fa044424
ksdfg <ksdfg123@gmail.com>
2019-12-30 12:59:32 +0530

    add review table, still not decided how to store actual review
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- Ara_Ara/models.py
+++ Ara_Ara/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=50, primary_key=True)
     password = models.CharField(max_length=100)
+    role = models.CharField(max_length=1, choices=(('r', "reviewer"), ('w', "weeb")), default='w')
 
     class Meta:
         db_table = 'User'
@@ -16,7 +17,15 @@ class Anime(models.Model):
     total_eps = models.IntegerField()
     aired_eps = models.IntegerField(null=True)
     status = models.CharField(max_length=1,
-                              choices=(('a', "airing"), ('n', "not yet aired"), ('f', "finished airing")))
+                              choices=(('a', "airing"), ('n', "not yet aired"), ('f', "finished airing")), default='n')
 
     class Meta:
         db_table = 'Anime'
+
+
+class Review(models.Model):
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Review'
