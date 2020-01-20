348f48567bb35340bc3f307600937def3fa51b68
ksdfg <ksdfg123@gmail.com>
2020-01-21 00:14:08 +0530

    set debug condition from config vars
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,8 @@ if os.path.exists(os.path.join(BASE_DIR, 'project', 'config.json')):
 else:
     try:
         data = {
-            "DATABASE_URL": os.environ["DATABASE_URL"]
+            "DATABASE_URL": os.environ["DATABASE_URL"],
+            "DEBUG": os.environ["DATABASE_URL"]
         }
     except KeyError:
         print("You don't have configuration JSON or environment variables set, go away")
@@ -39,9 +40,9 @@ else:
 SECRET_KEY = 'v-cb8xk=2!s1+3oh2fvsi7tc9(+p@!@b%mq_#$r+sm9rhtdmy8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = data["DEBUG"] == "True"
 
-ALLOWED_HOSTS = ["ara-ara-ufufu.herokuapp.com"]
+ALLOWED_HOSTS = ["ara-ara-ufufu.herokuapp.com", "127.0.0.1", "localhost"]
 
 # Application definition
 
